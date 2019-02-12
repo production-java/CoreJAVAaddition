843393b6ac69878b6065cff45a6f1f9704c94d5b
Developer <production@Developers-iMac.local>
2019-02-12 02:23:54 -0500

    CORE JAVA ADDITION

---
+++ src/com/corejava/adding/Adding.java
@@ -0,0 +1,18 @@
+package com.corejava.adding;
+
+public class Adding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+							
+				int myfirstNumber =  (10+5) + (2 + 10);
+				int mysecondNumber = 12;
+				int mythirdNumber =  6;
+				int myTotal = myfirstNumber + mysecondNumber + mythirdNumber;
+				
+				System.out.println(myTotal);
+
+	}
+
+}
